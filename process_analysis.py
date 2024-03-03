a0ccbe5af4c600e36b08071d8055ee4f49a1de9e
LukasK <lukas.koenig2@mailbox.tu-dresden.de>
2024-03-03 12:32:07 +0100

    Finds seasons/cycles based on autocorrelation, plots the data appropriately overlapped

--- process_analysis.py
+++ process_analysis.py
@@ -2,11 +2,8 @@ import pandas as pd
 import glob
 import numpy as np
 import pickle
-
-file_location = "\Dokumente\Privat\Plasway\Al2O3 Process Data"
-files = glob.glob(file_location+"\*.csv")
-skiprows = np.concatenate([np.arange(0,5), np.arange(7,24)])
-store = pd.HDFStore("good_process_spectra.h5")
+import matplotlib.pyplot as plt
+from scipy import signal
 
 def loadfiles(files):
     """
@@ -18,6 +15,7 @@ def loadfiles(files):
 
 def process_store(files):
     """
+    files: list of filepaths to read
     If store is still the full dataset as loaded, this will split it into a part dedicated to the spectrum (an in the process halfing the size of the file,
     because that is terribly memory inefficient and stores the utterly redundant information about pixel-wavelength assignment at every timestep) and a much
     smaller part dedicated to all other variables, which are also grouped according to their observation interval. 
@@ -29,7 +27,7 @@ def process_store(files):
         df.columns.get_loc("aWavelengthIntensity ")
     except KeyError:
         return
-    # Move the Spectrum to its own df's
+    # Move the Spectrum to its own df
     spec_intensities_start = df.columns.get_loc("aWavelengthIntensity ")
     spec_wavelengths_start = df.columns.get_loc("aWavelengthRange ")
     spec_wavelengths_end = df.columns.get_loc("O2Intnsty") - 1
@@ -46,15 +44,51 @@ def process_store(files):
     # structure: {7000: n*7000 df, 70000: n*70000 df, ...}
     for i in range(0, len(df.columns), 2):
         df_col = df.iloc[:,i+1]
-        df_col.dropna(inplace=True)
         df_col_timestamps = df.iloc[:,i]
-        df_col_timestamps.dropna(inplace=True)
+        df_col.index = df_col_timestamps    # set timestamps as indices instead
+        df_col.dropna(inplace=True)
         if df_col.size in datasets:
             datasets[df_col.size] = pd.concat([datasets[df_col.size], df_col], axis=1) # add the column to the dataframe of that length
         else:
-            datasets[df_col.size] = pd.concat([df_col_timestamps, df_col], axis=1) # Initialize the dictionary entry with the timescale specific to that length
+            datasets[df_col.size] = df_col # Initialize the dictionary entry
     # save data to files so the small part can be easily accessed
     with open('non_spectra.pkl', 'wb') as pickle_file:
         pickle.dump(datasets, pickle_file)
 
-process_store()
+
+
+def find_seasons(series:pd.Series):
+    """
+    Find indices in the data at which a pattern repeats using autocorrelation
+    """
+    min_time = 200 # minimum accepted cycle length in ms
+    autocorr = np.zeros(len(series))
+    # TODO find a way to avoid loops here
+    for i in range(len(series)):
+        autocorr[i] = series.autocorr(lag=i)
+    delta_t = series.index[1] - series.index[0]
+    min_distance = min_time/delta_t  
+    seasonal_indices = signal.find_peaks(autocorr, height=0.95, distance = min_distance, width=5)[0] # find the indices at the peaks of the autocorrelation
+    return series.index[seasonal_indices]
+
+def plot_seasons(series:pd.Series, season_times, ax):
+    for i in range(len(season_times)-1):
+        df_plot = series.loc[season_times[i]:season_times[i+1]]
+        ax.plot(df_plot.index - season_times[i], df_plot.values)
+
+def main():
+    file_location = "\Dokumente\Privat\Plasway\Al2O3 Process Data"
+    files = glob.glob(file_location+"\*.csv")
+    skiprows = np.concatenate([np.arange(0,5), np.arange(7,24)])
+    store = pd.HDFStore("good_process_spectra.h5")
+    with open('non_spectra.pkl', 'rb') as f:
+        loaded_dict = pickle.load(f)
+    SetPoint14 = loaded_dict[7110]["reaPositionSetPoint (14)"]
+    pressure_series = loaded_dict[71084]["reaActualPressure_mbar"]
+    season_times = find_seasons(SetPoint14)
+    fig = plt.figure()
+    ax = fig.subplots()
+    plot_seasons(pressure_series, season_times, ax)
+    plt.show()
+
+main()
