3234c4eac4ba054c6bbaea3bf396e0e53af171f4
LukasK <lukas.koenig2@mailbox.tu-dresden.de>
2024-03-03 17:25:01 +0100

    Plotting and some statistical analysis

--- process_analysis.py
+++ process_analysis.py
@@ -3,17 +3,18 @@ import glob
 import numpy as np
 import pickle
 import matplotlib.pyplot as plt
+from matplotlib.axes._axes import Axes
 from scipy import signal
+import os
 
-def loadfiles(files):
+def loadfiles(file, skiprows):
     """
-    load all files in the designated path into a very big df, store that for easier access later
+    load all files in the designated path into a very big df
     """
-    for file in files:
-        df = pd.read_csv(file, skiprows=skiprows, engine="c", header=0, na_values=[' '], dtype=np.float64)
-        return df
+    df = pd.read_csv(file, skiprows=skiprows, engine="c", header=0, na_values=[' '], dtype=np.float32)
+    return df
 
-def process_store(files):
+def process_store(files, skiprows, store):
     """
     files: list of filepaths to read
     If store is still the full dataset as loaded, this will split it into a part dedicated to the spectrum (an in the process halfing the size of the file,
@@ -21,7 +22,7 @@ def process_store(files):
     smaller part dedicated to all other variables, which are also grouped according to their observation interval. 
     should do nothing, if store was already processed
     """
-    df = loadfiles(files)
+    df = loadfiles(files, skiprows)
     # see if the df was processed already
     try:
         df.columns.get_loc("aWavelengthIntensity ")
@@ -71,11 +72,40 @@ def find_seasons(series:pd.Series):
     seasonal_indices = signal.find_peaks(autocorr, height=0.95, distance = min_distance, width=5)[0] # find the indices at the peaks of the autocorrelation
     return series.index[seasonal_indices]
 
-def plot_seasons(series:pd.Series, season_times, ax):
-    for i in range(len(season_times)-1):
-        df_plot = series.loc[season_times[i]:season_times[i+1]]
-        ax.plot(df_plot.index - season_times[i], df_plot.values)
+def plot_seasons(seasons, season_statistics, ax: Axes):
+    """Takes in a pd Series and some cyclicity determined elsewhere and plots it so that the seasons overlap
 
+    Args:
+        series (pd.Series): time-series of type Number
+        season_times (ndarray): Indices at which the seasons repeat
+        ax (Axes): plot area
+    """
+    # get the shortest season for initialising array for calculating mean value over seasons
+    
+    for i in seasons:
+        ax.plot(i, c='lightsteelblue', alpha = 0.7)
+    ax.plot(season_statistics, c='navy')
+
+def analyse_seasons(series:pd.Series, season_times):
+    shortest_season = np.argmin(np.diff(season_times))
+    shortest_season_indices = series.loc[season_times[shortest_season]:season_times[shortest_season+1]].index - season_times[shortest_season]
+    sum = np.zeros(len(shortest_season_indices))
+    seasons = []
+    season_number = len(season_times)-1
+    truncated_seasons = np.zeros((season_number, len(shortest_season_indices))) # to store truncated seasons to do quick statistical analysis
+    for i in range(season_number):
+        df_season = series.loc[season_times[i]:season_times[i+1]]
+        sum += df_season.values[:len(sum)]
+        df_season.index -= season_times[i]
+        seasons.append(df_season)
+        truncated_seasons[i,:] = df_season.values
+    mean = sum/season_number
+    analysis_df = pd.DataFrame(data=mean, index=shortest_season_indices)
+    var = np.sqrt(np.sum((truncated_seasons-mean)^2, axis=1)/season_number) # not quite the variance, quantifies the difference of the whole season from the mean
+    
+    return seasons, analysis_df
+        
+        
 def main():
     file_location = "\Dokumente\Privat\Plasway\Al2O3 Process Data"
     files = glob.glob(file_location+"\*.csv")
@@ -83,12 +113,14 @@ def main():
     store = pd.HDFStore("good_process_spectra.h5")
     with open('non_spectra.pkl', 'rb') as f:
         loaded_dict = pickle.load(f)
+    print("Opened file:{}".format(os.path.basename(files[0])))        
     SetPoint14 = loaded_dict[7110]["reaPositionSetPoint (14)"]
-    pressure_series = loaded_dict[71084]["reaActualPressure_mbar"]
     season_times = find_seasons(SetPoint14)
+    pressure_series = loaded_dict[71084]["reaActualPressure_mbar"]
     fig = plt.figure()
     ax = fig.subplots()
-    plot_seasons(pressure_series, season_times, ax)
+    seasons, mean_season = analyse_seasons(SetPoint14, season_times)
+    plot_seasons(seasons, mean_season, ax)
     plt.show()
 
 main()
