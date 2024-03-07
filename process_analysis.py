ab29d2034455f5242730601bce33be1f113994a9
LukasK <lukas.koenig2@mailbox.tu-dresden.de>
2024-03-07 11:35:49 +0100

    Fixed some problems with very singular series, prettier plot

--- process_analysis.py
+++ process_analysis.py
@@ -56,30 +56,29 @@ def process_store(file, skiprows, store):
         if df_col.size in datasets:
             datasets[df_col.size] = pd.concat([datasets[df_col.size], df_col], axis=1) # add the column to the dataframe of that length
         else:
-            datasets[df_col.size] = df_col # Initialize the dictionary entry
+            datasets[df_col.size] = pd.DataFrame(df_col) # Initialize the dictionary entry
     # save data to files so the small part can be easily accessed
     with open('non_spectra.pkl', 'wb') as pickle_file:
         pickle.dump(datasets, pickle_file)
 
 
-
-def find_seasons(series:pd.Series):
+def find_seasons(df:pd.DataFrame):
     """
-    Find indices in the data at which a pattern repeats using autocorrelation
+    Find indices in the data at which a pattern repeats using autocorrelation. Beginning and end are *not* included! TODO get the ends tucked in
     """
-    min_time = 200 # minimum accepted cycle length in ms
-    autocorr = np.zeros(len(series))
-    # TODO find a way to avoid loops here
-    autocorr = signal.correlate(series, series)[len(series)-1:]
-    delta_t = series.index[1] - series.index[0]
+    min_time = 500 # minimum accepted cycle length in ms
+    autocorr = np.zeros_like(df)
+    for i, col in enumerate(df.columns):
+        autocorr[:,i] = signal.correlate(df[col], df[col])[:df.shape[0]]
+    delta_t = df.index[1] - df.index[0]
     min_distance = min_time/delta_t
-    plt.plot(series)
-    #print(np.size(autocorr))
-    plt.plot(series.index, autocorr/np.max(autocorr))
-    plt.show()  
-    # Maybe find maximum number of peaks of identical maximum height, use that as seasons?
-    seasonal_indices = signal.find_peaks(autocorr, height=0.95, distance = min_distance, width=5)[0] # find the indices at the peaks of the autocorrelation
-    return series.index[seasonal_indices]
+    autocorr = autocorr/np.max(autocorr, axis=0)
+    seasonal_indices_old = []
+    for column in autocorr.T:
+        seasonal_indices = signal.find_peaks(column, distance = min_distance, width=min_distance/2)[0] # find the indices at the peaks of the autocorrelation
+        if len(seasonal_indices) > len(seasonal_indices_old):   # The most seasons are likely the most accurate
+            seasonal_indices_old = seasonal_indices
+    return df.index[seasonal_indices_old[:-2]] # last index is not really a cycle due to the tail of the recording
 
 def plot_seasons(seasons, season_statistics, ax: Axes):
     """Takes in a pd Series and some cyclicity determined elsewhere and plots it so that the seasons overlap
@@ -89,27 +88,28 @@ def plot_seasons(seasons, season_statistics, ax: Axes):
         season_times (ndarray): Indices at which the seasons repeat
         ax (Axes): plot area
     """
-    # get the shortest season for initialising array for calculating mean value over seasons
-    alpha_0 = 0.5
-    alpha_inc = (1-alpha_0)/len(seasons)
-    for i, s in enumerate(seasons):
-        ax.plot(s, c='lightsteelblue', alpha = alpha_0 + i*alpha_inc)
-    ax.plot(season_statistics, c='navy')
+    for s in seasons:
+        ax.plot(s)
+    ax.plot(season_statistics, c='navy', label="Mean of seasons")
 
 def analyse_seasons(series:pd.Series, season_times):
     shortest_season = np.argmin(np.diff(season_times))
     shortest_season_indices = series.loc[season_times[shortest_season]:season_times[shortest_season+1]].index - season_times[shortest_season]
-    sum = np.zeros(len(shortest_season_indices))
-    seasons = []
+    # Include the non-seasoned data as pre-and postlude
+    pre_run = series.loc[series.index[0]:season_times[0]]
+    pre_run.index -= season_times[0]
+    post_run = series.loc[season_times[-1]:series.index[-1]]
+    post_run.index -= season_times[-1]
+    seasons = [pre_run]
     season_number = len(season_times)-1
     truncated_seasons = np.zeros((season_number, len(shortest_season_indices))) # to store truncated seasons to do quick statistical analysis
     for i in range(season_number):
         df_season = series.loc[season_times[i]:season_times[i+1]]
-        sum += df_season.values[:len(sum)]
         df_season.index -= season_times[i]
         seasons.append(df_season)
-        truncated_seasons[i,:] = df_season.values
-    mean = sum/season_number
+        truncated_seasons[i,:] = df_season.values[:len(shortest_season_indices)]
+    mean = np.mean(truncated_seasons, axis=0)
+    seasons.append(post_run)
     analysis_df = pd.DataFrame(data=mean, index=shortest_season_indices)
     deviation_from_mean = truncated_seasons - mean
     var = np.sqrt(np.sum(deviation_from_mean**2, axis=1)/season_number) # not quite the variance, quantifies the difference of the whole season from the mean
@@ -124,13 +124,16 @@ def display_data(loaded_dict:dict, dict_key:int, table_key:str, season_times):
         table_key (_type_): which column of the data frame is interesting
         season_times (_type_): the times at which cycles in the data repeat
     """
+    series = loaded_dict[dict_key][table_key]
+    seasons, mean_season = analyse_seasons(series, season_times)
+    plt.rcParams["axes.prop_cycle"] = plt.cycler("color", plt.cm.cool(np.linspace(0.01,1,len(seasons))))
     fig = plt.figure()
     ax = fig.subplots()
     ax.set_xlabel("t/ms")
     ax.set_title(table_key)
-    series = loaded_dict[dict_key][table_key]
-    seasons, mean_season = analyse_seasons(series, season_times)
+    ax.set_xlim(mean_season.index[0] - 20, mean_season.index[-1] + 20)
     plot_seasons(seasons, mean_season, ax)
+    ax.legend()
     plt.show()
     
 def enter_file(file_location=None):
@@ -166,24 +169,27 @@ def main():
                 break
         skiprows = np.concatenate([np.arange(0,5), np.arange(7,24)])
         store = pd.HDFStore("good_process_spectra.h5")
-        process_store(filepath, skiprows, store) # only for loading a new file!
+        process_store(filepath, skiprows, store) 
 
     with open('non_spectra.pkl', 'rb') as f:
         loaded_dict = pickle.load(f)
     #print("Opened file: {}".format(os.path.basename(files[0])))
     order_df_key = min(list(loaded_dict.keys()))       
-    season_gauge = loaded_dict[order_df_key].iloc[:,4]
-    print(loaded_dict[order_df_key].columns)
+    season_gauge = loaded_dict[order_df_key]
     with warnings.catch_warnings(action="ignore"):
         season_times = find_seasons(season_gauge)
     while True:
         print("available measurement series sizes: {}".format(list(loaded_dict.keys())))
-        dict_key = int(input("Which series do you want to consider? "))
+        dict_key = (input("Which series do you want to consider? "))
         try: 
-            loaded_dict[dict_key]
+            loaded_dict[int(dict_key)]
         except KeyError:
             print("Please choose a valid key.")
             continue
+        except ValueError:
+            print("Please choose a numerical key.")
+            continue
+        dict_key = int(dict_key)
         print("Measurements in this series:")
         for i in sorted(list(loaded_dict[dict_key])):
             print(i)
